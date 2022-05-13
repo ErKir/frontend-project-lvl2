ec26f21ade5d48bc241d06bfb950f97682ff834b
kirill <kirillerohin@gmail.com>
2022-05-13 22:56:29 +0300

    feat: implement comander.js

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 install:
 	npm ci
 
-brain-games:
-	node bin/games/brain-games.js
-
 publish:
 	npm publish --dry-run
+
+gendiff:
+	node gendiff.js
 
 lint:
 	npx eslint .
--- gendiff.js
+++ gendiff.js
@@ -1,5 +1,23 @@
+#!/usr/bin/env node
+
 import {
-  Command
+  Command,
 } from 'commander';
 
 const program = new Command();
+
+// mast be export as default!
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
+
+// program.command()
+//   .option('-V, --version', 'output the version number')
+//   .action((str, options) => {
+//     const limit = options.first ? 1 : undefined;
+//     console.log(str.split(options.separator, limit));
+//   });
+
+program.parse();
