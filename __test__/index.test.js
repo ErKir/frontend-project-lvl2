b3f2c4a441ff1970d41e968a51d49231ac4ea837
kirill <kirillerohin@gmail.com>
2022-07-12 15:17:37 +0300

    feat: add tests & files for yaml

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -17,11 +17,19 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
-const actual1 = getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-const expected1 = readFile('result.json').trim();
-console.log('exp==', expected1);
-console.log('act==', actual1);
 
 test('result must be equal "result.json"', () => {
+  const actual = getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  const expected = readFile('result.json').trim();
+  // console.log('exp==', expected1);
+  // console.log('act==', actual1);
+  expect(actual).toEqual(expected);
+});
+
+test('result must be equal "resultYML.yml"', () => {
+  const actual1 = getDiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'));
+  const expected1 = readFile('resultYML.yml').trim();
+  console.log('exp==', expected1);
+  console.log('act==', actual1);
   expect(actual1).toEqual(expected1);
 });
