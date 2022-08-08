f47b7303e00cdb67220308de7a56136a1f047d8f
kirill <kirillerohin@gmail.com>
2022-08-08 16:56:58 +0300

    ref: edit fixtyres & tests

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -19,17 +19,17 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('result must be equal "result.json"', () => {
+test('JSON file test', () => {
   const actual = getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const expected = readFile('result.json').trim();
+  const expected = readFile('result.txt').trim();
   // console.log('exp==', expected1);
   // console.log('act==', actual1);
   expect(actual).toEqual(expected);
 });
 
-test('result must be equal "resultYML.yml"', () => {
+test('YAML file test', () => {
   const actual = getDiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'));
-  const expected = readFile('result.json').trim();
+  const expected = readFile('result.txt').trim();
   // console.log('exp==', expected1);
   // console.log('act==', actual1);
   expect(actual).toEqual(expected);
