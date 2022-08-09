36711f468f051cb0b4ed3963e66bb4ff0233697a
kirill <kirillerohin@gmail.com>
2022-08-09 23:30:04 +0300

    ref: add stylish as default format

--- main.js
+++ main.js
@@ -6,18 +6,20 @@ import {
 import getDiff from './bin/getDiff.js';
 
 const program = new Command();
-
+console.log(`Current directory: ${process.cwd()}`);
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0');
 
 program
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath_1>')
   .argument('<filepath_2>')
-  .action((filepath1, filepath2, option) => getDiff(filepath1, filepath2, option.format));
-// .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2,
-// program.opts().format)))
+  .action((filepath1, filepath2) => console.log(
+    getDiff(filepath1, filepath2, program.opts().format),
+  ));
 // .parse(process.argv);
 program.parse();
+
+// консолить нужно от сюда, что бы в библиотеке небыло консолей
