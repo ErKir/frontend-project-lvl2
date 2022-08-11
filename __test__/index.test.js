d09e48c52f6550600dd5914b22690a0f550e4cde
kirill <kirillerohin@gmail.com>
2022-08-11 20:35:26 +0300

    feat: add test for plain formater

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -8,7 +8,6 @@ import {
 import {
   readFileSync,
 } from 'fs';
-// import yaml from 'js-yaml';
 import getDiff from '../bin/getDiff.js';
 
 const __filename = fileURLToPath(
@@ -19,18 +18,16 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('JSON file test', () => {
-  const actual = getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish');
-  const expected = readFile('resultStylish.txt').trim();
-  // console.log('exp==', expected1);
-  // console.log('act==', actual1);
+const formats = ['stylish', 'plain'];
+
+test.each(formats)('JSON file test with %s output format', (format) => {
+  const actual = getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), format);
+  const expected = readFile(`${format}Result.txt`).trim();
   expect(actual).toEqual(expected);
 });
 
-test('YAML file test', () => {
-  const actual = getDiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'), 'stylish');
-  const expected = readFile('resultStylish.txt').trim();
-  // console.log('exp==', expected1);
-  // console.log('act==', actual1);
+test.each(formats)('YAML file test with %s output format', (format) => {
+  const actual = getDiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'), format);
+  const expected = readFile(`${format}Result.txt`).trim();
   expect(actual).toEqual(expected);
 });
