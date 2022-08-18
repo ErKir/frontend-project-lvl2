5962dd89ab991b1725f099ebf2690788a749c5ba
kirill <kirillerohin@gmail.com>
2022-08-18 23:48:06 +0300

    ref: add new key 'nested' for tree

--- src/builderTree.js
+++ src/builderTree.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 // array You Shall Not Pass!!!
-const isObject = (obj) => Object.prototype.toString.call(obj) === '[object Object]';
+// const isObject = (obj) => Object.prototype.toString.call(obj) === '[object Object]';
 
 const builder = (obj1, obj2) => {
   const keys1 = _.keys(obj1);
@@ -9,44 +9,42 @@ const builder = (obj1, obj2) => {
   const arrayUniqKeys = _.sortBy(_.union(keys1, keys2));
   // building diff as array with next data {key, value, event},
   // event can accept state:(deleted || added || unchanged || updated)
-  const diff = arrayUniqKeys.reduce((acc, key) => {
+  const diff = arrayUniqKeys.map((key) => {
     if (_.has(obj1, key) && _.has(obj2, key)) {
-      if (isObject(obj1[key]) && isObject(obj2[key])) {
-        return [...acc, {
+      if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+        return {
           name: key,
           value: builder(obj1[key], obj2[key]),
-          event: 'unchanged',
-        }];
+          type: 'nested',
+        };
       }
-      if (obj1[key] === obj2[key]) {
-        return [...acc, {
+      if (_.isEqual(obj1[key], obj2[key])) {
+        return {
           name: key,
           value: obj1[key],
-          event: 'unchanged',
-        }];
+          type: 'unchanged',
+        };
       }
-      return [...acc,
-        {
-          name: key,
-          value1: obj1[key],
-          value2: obj2[key],
-          event: 'updated',
-        },
-      ];
+      return {
+        name: key,
+        value1: obj1[key],
+        value2: obj2[key],
+        type: 'updated',
+      };
     }
     if (!_.has(obj2, key)) {
-      return [...acc, {
+      return {
         name: key,
         value: obj1[key],
-        event: 'removed',
-      }];
+        type: 'removed',
+      };
     }
-    return [...acc, {
+    return {
       name: key,
       value: obj2[key],
-      event: 'added',
-    }];
-  }, []);
+      type: 'added',
+    };
+  });
   return diff;
 };
 
