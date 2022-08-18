77bd024092b21a80c112e0e823c64283e7adbba5
kirill <kirillerohin@gmail.com>
2022-08-18 23:54:36 +0300

    ref: del comments

--- src/builderTree.js
+++ src/builderTree.js
@@ -1,14 +1,11 @@
 import _ from 'lodash';
 
-// array You Shall Not Pass!!!
-// const isObject = (obj) => Object.prototype.toString.call(obj) === '[object Object]';
-
 const builder = (obj1, obj2) => {
   const keys1 = _.keys(obj1);
   const keys2 = _.keys(obj2);
   const arrayUniqKeys = _.sortBy(_.union(keys1, keys2));
-  // building diff as array with next data {key, value, event},
-  // event can accept state:(deleted || added || unchanged || updated)
+  // building diff as array with next data {key, value, type},
+  // type can accept state:(deleted || added || unchanged || updated || nested)
   const diff = arrayUniqKeys.map((key) => {
     if (_.has(obj1, key) && _.has(obj2, key)) {
       if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
